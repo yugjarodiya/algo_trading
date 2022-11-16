dbdd76951b7ca9841c1f5156b55913ee32bfaf55
Yug Jarodiya <jarodiyayug@gmail.com>
2022-11-15 23:18:29 -0500

    working

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,13 +3,38 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
   const [currentTime, setCurrentTime] = useState(0);
+  const [name, setName] = useState('Default')
+  
 
   useEffect(() => {
     fetch('/time').then(res => res.json()).then(data => {
       setCurrentTime(data.time);
     });
   }, []);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    console.log("making request")
+
+   fetch("/result", {
+        method:"POST",
+        cache: "no-cache",
+        headers:{
+            "content_type":"application/json",
+        },
+        body:JSON.stringify(name)
+        }
+    ).then(response => {
+    return response.json()
+  })
+  .then(json => {
+    console.log(json[0])
+    //this.setState({playerName: json[0]})
+  })
+  }
 
   return (
     <div className="App">
@@ -18,6 +43,22 @@ function App() {
         ... no changes in this part ...
 
         <p>The current time is {currentTime}.</p>
+        <p>{name}</p>
+
+
+        {/* This is the form */}
+        <form onSubmit={handleSubmit}>
+          <label>Enter your name:
+          <input 
+            type="text" 
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          </label>
+          <input type="submit" />
+        </form>
+
+
       </header>
     </div>
   );
