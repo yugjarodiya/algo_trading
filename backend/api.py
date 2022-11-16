264cc988b070ead6715144c40f7f7086e15b8b3c
Yug Jarodiya <jarodiyayug@gmail.com>
2022-11-15 23:19:05 -0500

    working

--- backend/api.py
+++ backend/api.py
@@ -1,15 +1,23 @@
 import requests
 import json
 import yfinance as yf
-from flask import Flask
+from flask import Flask, jsonify
 from datetime import datetime
 import pandas as pd
+#import request
 
 
 
 
 
 app = Flask(__name__)
+
+# @app.route("/send", methods=["GET", "POST"])
+# def send():
+#     if request.method == "POST":
+#         title = str(request.json["title"])
+#         body = str(request.json["body"])
+#         return jsonify("Sended")
 
 @app.route('/time')
 def get_current_time():
@@ -24,4 +32,14 @@ def get_current_time():
     for l in df:
         count+=1
     return str(count)
+
+@app.route('/result', methods = ['POST'])
+def result():
+    # player_id = request.json
+    # if player_id:
+    #    data = get_player(player_id)
+    #    name = str(data['name'][0])
+    name = "HelloIamGay"
+    return jsonify(name)
+    #return "No player information is given"
     
