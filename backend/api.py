e2a5455c892b3defd1be76e8bf622b1f087eec2f
Yug Jarodiya <jarodiyayug@gmail.com>
2022-11-16 18:30:00 -0500

    working flask

--- backend/api.py
+++ backend/api.py
@@ -1,7 +1,6 @@
-import requests
 import json
 import yfinance as yf
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from datetime import datetime
 import pandas as pd
 #import request
@@ -12,6 +11,23 @@ import pandas as pd
 
 app = Flask(__name__)
 
+@app.route("/username", methods=['POST'])
+def login():
+    # username = request.form.get("username", "") # or request.form["username"]
+    title = request.json['username']
+        # CREATE TICKER INSTANCE FOR AMAZON
+    amzn = yf.Ticker(str(title))
+    #amzn = yf.Ticker("AMZN")
+
+    # GET TODAYS DATE AND CONVERT IT TO A STRING WITH YYYY-MM-DD FORMAT (YFINANCE EXPECTS THAT FORMAT)
+    end_date = datetime.now().strftime('%Y-%m-%d')
+    amzn_hist = amzn.history(start='2022-01-01',end=end_date)
+    df =amzn_hist.values.tolist()
+    return jsonify(str(df))
+
+        #return jsonify(username=username) # from flask import jsonify 
+    return jsonify(title)
+
 # @app.route("/send", methods=["GET", "POST"])
 # def send():
 #     if request.method == "POST":
@@ -19,27 +35,16 @@ app = Flask(__name__)
 #         body = str(request.json["body"])
 #         return jsonify("Sended")
 
-@app.route('/time')
-def get_current_time():
-    # CREATE TICKER INSTANCE FOR AMAZON
-    amzn = yf.Ticker("AMZN")
 
-    # GET TODAYS DATE AND CONVERT IT TO A STRING WITH YYYY-MM-DD FORMAT (YFINANCE EXPECTS THAT FORMAT)
-    end_date = datetime.now().strftime('%Y-%m-%d')
-    amzn_hist = amzn.history(start='2022-01-01',end=end_date)
-    df =amzn_hist.values.tolist()
-    count = 0
-    for l in df:
-        count+=1
-    return str(count)
 
-@app.route('/result', methods = ['POST'])
-def result():
-    # player_id = request.json
-    # if player_id:
-    #    data = get_player(player_id)
-    #    name = str(data['name'][0])
-    name = "HelloIamGay"
-    return jsonify(name)
-    #return "No player information is given"
+# @app.route('/result', methods = ['POST'])
+# def result():
+#     # player_id = request.json
+#     # if player_id:
+#     #    data = get_player(player_id)
+#     #    name = str(data['name'][0])
+#     name = "HelloIamGay"
+#     return jsonify(name)
+#     #return "No player information is given"
+
     
--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,66 +1,70 @@
 import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+
 
 function App() {
 
-  const [currentTime, setCurrentTime] = useState(0);
-  const [name, setName] = useState('Default')
+  // const [currentTime, setCurrentTime] = useState(0);
+  // const [name, setName] = useState('Default')
   
 
-  useEffect(() => {
-    fetch('/time').then(res => res.json()).then(data => {
-      setCurrentTime(data.time);
-    });
-  }, []);
+  // useEffect(() => {
+  //   fetch('/time').then(res => res.json()).then(data => {
+  //     setCurrentTime(data.time);
+  //   });
+  // }, []);
+  const [user, setUser] = useState("");
+  const login = (e) => {
+  e.preventDefault();
+  axios
+      .post("/username", {
+          // username: document.getElementById("username").value,
+          'username': user,
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
+        })
+        .then((res) => {
+            console.log( res.data);
+        });
+};
 
-    console.log("making request")
 
-   fetch("/result", {
-        method:"POST",
-        cache: "no-cache",
-        headers:{
-            "content_type":"application/json",
-        },
-        body:JSON.stringify(name)
-        }
-    ).then(response => {
-    return response.json()
-  })
-  .then(json => {
-    console.log(json[0])
-    //this.setState({playerName: json[0]})
-  })
-  }
 
   return (
-    <div className="App">
-      <header className="App-header">
+    // <div className="App">
+    //   <header className="App-header">
 
-        ... no changes in this part ...
+    //     ... no changes in this part ...
 
-        <p>The current time is {currentTime}.</p>
-        <p>{name}</p>
-
-
-        {/* This is the form */}
-        <form onSubmit={handleSubmit}>
-          <label>Enter your name:
-          <input 
-            type="text" 
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
-          </label>
-          <input type="submit" />
-        </form>
+    //     <p>The current time is {currentTime}.</p>
+    //     <p>{name}</p>
 
 
-      </header>
-    </div>
+    //     {/* This is the form */}
+    //     <form onSubmit={handleSubmit}>
+    //       <label>Enter your name:
+    //       <input 
+    //         type="text" 
+    //         value={name}
+    //         onChange={(e) => setName(e.target.value)}
+    //       />
+    //       </label>
+    //       <input type="submit" />
+    //     </form>
+
+
+    //   </header>
+    // </div>
+    <form onSubmit={login} method="post">
+    <p>
+       <label htmlFor="email">Email</label>
+       <input onChange={(e) => setUser(e.target.value)} type="username"  id="username" name="username" />
+   </p>
+   <p>
+      <input type="submit" className="w3-button w3-blue" value="Login" />
+   </p>
+</form>
   );
 }
 
